a8c2748173b257a34b39ca8c6220bf24fc4c3cf1
Carlos Kirui <carloskirui154@gmail.com>
2023-08-19 14:29:24 -0400

    modified the app.js

--- app.js
+++ app.js
@@ -22,9 +22,6 @@ connectToDb((err) => {
 
 // routes
 app.get("/books", (req, res) => {
-  // current page
-  const page = req.query.p || 0;
-  const booksPerPage = 3;
 
 
   let books = [];
@@ -32,8 +29,6 @@ app.get("/books", (req, res) => {
   db.collection("books")
     .find()
     .sort({ author: 1 })
-    .skip(page * booksPerPage)
-    .limit(booksPerPage)
     .forEach((book) => books.push(book))
     .then(() => {
       res.status(200).json(books);
